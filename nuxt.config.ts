c264311a6299ce45c8197e8747f2743af3ea5938
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-11-21 17:19:36 +0530

    Added gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@vueuse/motion/nuxt", "@nuxt/eslint", "@nuxt/fonts"],
+  modules: ["@nuxt/image", "@vueuse/motion/nuxt", "@nuxt/eslint", "@nuxt/fonts", "nuxt-gtag"],
   components: [
     {
       path: "~/components",
