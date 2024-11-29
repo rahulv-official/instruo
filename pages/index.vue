484888d8faaf1b190df7ecf616fff92706ee24e5
Rahul Vashishtha <rahulvashistha96@gmail.com>
2024-11-29 13:25:06 +0530

    Added SEO Meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
   <HeroSection></HeroSection>
 </template>
+
+<script lang="ts" setup>
+const config = useConfig();
+
+useSeoMeta({
+  title: `instruo.org: Open-source collection of tools and utilities.`,
+  ogTitle: `instruo.org: Open-source collection of tools and utilities.`,
+  description: "Open-source collection of tools and utilities.",
+  ogDescription: "Open-source collection of tools and utilities.",
+  ogImage: config.value.site.ogImage,
+  twitterCard: "summary_large_image",
+});
+</script>
